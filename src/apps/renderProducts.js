c92a53f15c73c0e01380b76142a4d572daf07f90
frim_mnero <frimmnero@gmail.com>
2024-02-13 19:32:21 +0300

    check

---
+++ src/apps/renderProducts.js
@@ -0,0 +1,33 @@
+const cardContainer = document.querySelector(".product__item_card");
+
+getCard();
+
+async function getCards(){
+
+    const response = await fetch('./src/apps/products.json');
+    console.log(response);
+
+    const cardArray = await response.json();
+    console.log(cardArray);
+
+    renderCards(cardArray);
+}
+
+function renderCards(cardArray) {
+    cardArray.forEach(function(element) {
+        const cardHTML = `
+                        <div class="product__item_card">
+                                <img src="${element.image}" alt="" class="item_card-img">
+                                <p class="item_card-title">Коммутатор Allied Telesyn AT-8124XL</p>
+                                <div class="item_card-buy">
+                                    <button class="item_card-btn">Забронировать</button>
+                                    <p class="item_card-price">53 000 ₽</p>
+                                </div>
+                            </div>
+                        `;
+   
+        
+    cardContainer.insertAdjacentHTML('beforeend',cardHTML);
+    });
+
+}
